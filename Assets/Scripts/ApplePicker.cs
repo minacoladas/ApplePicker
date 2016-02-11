ee561049140ad8ea0a0e97a6bad0b01258195c8a
Mina Zakhary <obeythepharaoh@gmail.com>
2016-02-11 17:49:30 -0500

    Almost Done Game
    Committing most of the game in changes

---
+++ Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplePicker : MonoBehaviour {
+	public GameObject basketPrefab;
+	public int numbaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSpacingY = 2f;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < numbaskets; i++) {
+			GameObject tBasketGO = Instantiate (basketPrefab) as GameObject;
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			tBasketGO.transform.position = pos;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
